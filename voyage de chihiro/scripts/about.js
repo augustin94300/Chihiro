9e2931858443fa17bc03be17ecd791888465faba
ThirietOscar <47737805+ThirietOscar@users.noreply.github.com>
2019-10-10 17:52:12 +0200

    ajout code
    animations sur page info

---
+++ voyage de chihiro/scripts/about.js
@@ -0,0 +1,21 @@
+const about_text = document.querySelector("#about_text")
+const text_storyLine = document.querySelector("#text_storyLine")
+const aboutTheCast = document.querySelector(".aboutTheCast")
+const movieDirector = document.querySelector(".movieDirector")
+window.addEventListener(
+    "scroll",
+    function (){
+      const scrolled = window.scrollY
+  
+      if(scrolled>175){
+        about_text.classList.add("animateMe")
+      }
+      if (scrolled > 1000){
+          aboutTheCast.classList.add("animateMe")
+          text_storyLine.classList.add("animateMe")
+      }
+      if(scrolled > 1850){
+          movieDirector.classList.add("animateMe")
+      }
+    }
+  )
