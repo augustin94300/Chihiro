84ae59c2fd295953d1dfeec490940f5c35bb61e7
sibylleHrvt <47738070+sibylleHrvt@users.noreply.github.com>
2019-10-10 17:21:59 +0200

    chgmt image

---
+++ voyage de chihiro/scriptes/accueil.js
@@ -0,0 +1,15 @@
+let sections = document.querySelector('.sections')
+let video = document.querySelector('.video')
+
+let isPaused = false;
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.intersectionRatio!=1  && !video.paused){
+      video.pause(); isPaused = true;
+    }
+    else if(isPaused) {video.play(); isPaused=false}
+  });
+}, {threshold: 1});
+observer.observe(video);
+
+let rellax = new Rellax('.rellax');
