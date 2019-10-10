80435c84895f206f54191069c82408eb60e4f11b
SandroHerveRaspaldo <sandroherveraspaldo@gmail.com>
2019-10-10 15:33:33 +0200

    Information page
    Information page upload and minor file management

--- voyage de chihiro/scriptes/accueil.js
+++ voyage de chihiro/scriptes/accueil.js
@@ -1,38 +1,15 @@
-let monstre = document.querySelector('.monstre')
-  let bebe = document.querySelector('.bebe')
-  let y = 0
-  let z =1
+let sections = document.querySelector('.sections')
+let video = document.querySelector('.video')
 
-  window.addEventListener("load", function(event) {
-    console.log("fr")
-    setInterval(function() {
+let isPaused = false;
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.intersectionRatio!=1  && !video.paused){
+      video.pause(); isPaused = true;
+    }
+    else if(isPaused) {video.play(); isPaused=false}
+  });
+}, {threshold: 1});
+observer.observe(video);
 
-      if (y=0){
-      let x = setTimeout( function() {
-      monstre.style.marginleft = '100px'
-      console.log('ok')
-      y=2;}, 1000)}
-
-      if (y=1){
-      let x = setTimeout( function() {
-      monstre.style.marginLeft = '200px'
-      console.log('ok')
-      y=2;}, 2000)}
-
-      if(y=2){
-      let x = setTimeout( function() {
-      monstre.style.marginLeft = '300px'
-      console.log('ok')
-      y=3;}, 3000)}
-
-      if(y=3){
-      let x = setTimeout( function() {
-      monstre.style.marginLeft = '200px'
-      console.log('ok')
-      y=3;}, 4000)}
-
-      if (y = 4) {
-      y=0}
-    }, 12000);
-
-    })
+let rellax = new Rellax('.rellax');
